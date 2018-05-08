5b89cc8defe62a2db26e00caf82a0ce90df3063c
Cristian <cristhian.toro@upb.edu.co>
2018-05-08 14:29:06 -0500

    adición pokemon y mejora vista desktop

--- pokedex-angular-servicios/src/app/app.module.ts
+++ pokedex-angular-servicios/src/app/app.module.ts
@@ -4,9 +4,7 @@ import { routing, appRoutingProviders } from './app.routing';
 import { HttpModule, Http } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
 import { HomeComponent } from './home/home.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 
@@ -14,9 +12,7 @@ import { PokemonComponent } from './pokemon/pokemon.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     HeaderComponent,
-    MainComponent,
     HomeComponent,
     PokemonComponent
   ],
--- pokedex-angular-servicios/src/app/app.routing.ts
+++ pokedex-angular-servicios/src/app/app.routing.ts
@@ -1,7 +1,6 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {MainComponent} from './main/main.component';
 import { HomeComponent } from './home/home.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 
@@ -10,8 +9,6 @@ const appRoutes: Routes = [
         { path:'', component:HomeComponent },
         { path:'home', component:HomeComponent },
         { path:'pokemon/:ident', component:PokemonComponent },
-        { path:'main', component:MainComponent},
-        // { path:'**', component:FrutasComponent }
     ];
     
 export const appRoutingProviders: any = [];
